19bf088941026ce4a78f934af788a8b112c0f1a9
ianmaboimonene <ian.maboi@student.moringaschool.com>
2023-09-24 13:09:33 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+const employee ={
+    name : "Ian Maboi",
+    streetAddress : "3144 Thika Main St",
+}
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return {...employee,[key]: value};
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee ,key ,value){
+    employee[key]=value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee,key){
+    const newEmployee ={ ...employee};
+    delete employee [key];
+    return newEmployee;
+}
+function destructivelyDeletefromEmployeeByKeyAndValue(employee, key){
+     const newEmployee = {...employee};
+     delete newEmployee[key];
+     return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
+const updateEmployee = updateEmployeeWithKeyAndValue(employee,"name","Sam smith","456 Elm St");
+    console.log(destructivelyUpdatedEmployee);
+
+const employeeWithoutStreet=deleteFromEmployeeByKey(employee, "streetAddress");
+    console.log(employeeWithoutStreet);
+    
+const destructivelyRemovedStreet =destructivelyDeletefromEmployeeByKeyAndValue(employee,"StreetAddress");
+    console.log(destructivelyRemovedStreet);    
+
+
